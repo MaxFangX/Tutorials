e5775415625ccb8301ec4409e5d7fff62e2a885f
MaxFangX <maxfangx@gmail.com>
2014-12-25 15:12:19 -0600

    Customize admin panel fields
    (Reorder pub_date before question_text

--- tutorial/polls/admin.py
+++ tutorial/polls/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from polls.models import Question
 
 # Register your models here.
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+	fields = ['pub_date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
