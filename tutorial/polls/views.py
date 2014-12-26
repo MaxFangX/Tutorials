a9f4f6511e297c96d013a11d2bddc1bce4407813
MaxFangX <maxfangx@gmail.com>
2014-12-26 15:03:17 -0600

    Implement Http404 for nonexistent questions

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import RequestContext, loader #Not needed
 from polls.models import Question
 
@@ -9,7 +9,11 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
