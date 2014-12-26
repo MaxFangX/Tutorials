5339d7307a08c0d709b4d923382a7369c26b5f21
MaxFangX <maxfangx@gmail.com>
2014-12-26 13:26:24 -0600

    Create simplest view

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+# Might be able to delete the line above
+#
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
