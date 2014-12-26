7f14c7d8c2df484ead2f15c4d7e87e593c9597ea
MaxFangX <maxfangx@gmail.com>
2014-12-26 14:32:54 -0600

    Create a view that does something

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 # Might be able to delete the line above
-#
+
 from django.http import HttpResponse
-# Create your views here.
+from polls.models import question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([p.question_text for p in latest_question_list])
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
