1cfea6bcfa127b33f0d3136521f372fda4d5cae9
MaxFangX <maxfangx@gmail.com>
2014-12-26 15:10:04 -0600

    Implemented same thing with Django shortcut

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse
 from django.template import RequestContext, loader #Not needed
 from polls.models import Question
 
@@ -9,10 +9,7 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
