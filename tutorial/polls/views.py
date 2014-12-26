25b1f14ddc281bb9ae94089022e68cf566c8a187
MaxFangX <maxfangx@gmail.com>
2014-12-26 14:52:07 -0600

    Polls index implemented with render() shortcut

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -1,17 +1,12 @@
 from django.shortcuts import render
-# Might be able to delete the line above
-
 from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.template import RequestContext, loader #Not needed
 from polls.models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'latest_question_list': latest_question_list,
-    })
-    return HttpResponse(template.render(context))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
