dff6cf00bbf9c2e931c3677fc6d26e9375052e2f
Max Fang <maxfangx@gmail.com>
2015-08-29 23:06:04 -0700

    Update action

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -25,6 +25,16 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   private
     def article_params
         params.require(:article).permit(:title, :text)
