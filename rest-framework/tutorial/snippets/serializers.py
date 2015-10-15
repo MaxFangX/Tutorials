213a8c7a40a3de7e1281e3dfd564bfb86c0095c0
Max Fang <maxfangx@gmail.com>
2015-10-14 23:39:24 -0700

    Fix typo

--- rest-framework/tutorial/snippets/serializers.py
+++ rest-framework/tutorial/snippets/serializers.py
@@ -4,7 +4,7 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 class SnippetSerializer(serializers.Serializer):
     pk = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(require = False, allow_blank=True, max_length=100)
+    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
     code = serializers.CharField(style={'base_template': 'textarea.html'})
     linenos = serializers.BooleanField(required=False)
     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
