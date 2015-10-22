e230fe3996e7b5ef1833c79a4f6cc78bb44e59ef
Max Fang <maxfangx@gmail.com>
2015-10-21 23:58:10 -0700

    Add new fields to Snippet model

--- rest-framework/tutorial/snippets/models.py
+++ rest-framework/tutorial/snippets/models.py
@@ -16,3 +16,5 @@ class Snippet(models.Model):
     linenos = models.BooleanField(default=False)
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
     style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+    owner = models.ForeignKey('auth.User', related_name='snippets')
+    highlighted = models.TextField()
