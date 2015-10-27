102b76ff404deb0e03f656b74acfbe38c3486b3f
Max Fang <maxfangx@gmail.com>
2015-10-27 00:41:25 -0700

    Create root api endpoint

--- rest-framework/tutorial/snippets/views.py
+++ rest-framework/tutorial/snippets/views.py
@@ -1,8 +1,18 @@
 from django.contrib.auth.models import User
 from rest_framework import generics, permissions
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
+
+
+@api_view(('GET',))
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
 
 
 class SnippetList(generics.ListCreateAPIView):
