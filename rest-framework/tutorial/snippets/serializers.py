a7eb65c8ae9a9736dd66d2a0e95efc5a14181ce9
Max Fang <maxfangx@gmail.com>
2015-10-29 02:12:07 -0700

    Hyperlinked serializer for Snippets

--- rest-framework/tutorial/snippets/serializers.py
+++ rest-framework/tutorial/snippets/serializers.py
@@ -3,13 +3,17 @@ from rest_framework import serializers
 from snippets.models import Snippet
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
 
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedRelatedField(view_name='snippet-highlight',
+                                                    format='html')
     
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner')
+        fields = ('url', 'highlight', 'owner',
+                  'title', 'code', 'linenos',
+                  'language', 'style')
 
 
 class UserSerializer(serializers.ModelSerializer):
